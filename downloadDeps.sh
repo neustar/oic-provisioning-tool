fa0b2faeb6dedf168c6a1b425e2ca9f6155f5b1e
Siu, David <david@neustar.biz>
2016-09-23 09:41:44 -0700

    Pinning to an older version of iotivity-contrained to pass build

--- downloadDeps.sh
+++ downloadDeps.sh
@@ -2,7 +2,16 @@
 
 # iotivity-constrained
 rm -rf lib/iotivity
-git clone https://gerrit.iotivity.org/gerrit/p/iotivity-constrained.git lib/iotivity
+git clone https://github.com/iotivity/iotivity-constrained.git lib/iotivity
+
+#
+# HACK: Pin to this version of iotivity-contrained  because changes are breaking the build.
+#       (Its a good idea to pin all dependency versions anyways :P)
+#
+cd lib/iotivity
+git reset --hard a79077f9646edc0814b5800ef49be9d73178a220
+cd ../..
+
 
 # TinyCBOR...
 git clone https://github.com/01org/tinycbor.git lib/iotivity/deps/tinycbor
